f27fdca7028fb9fcc830b24e1a7cb6e690091565
David Janes <david@consensas.com>
2018-04-18 15:11:33 +0200

    comprehend

---
+++ ecs/_util.js
@@ -0,0 +1,20 @@
+/**
+ *  ecs/_util.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2018-03-07
+ *
+ *  Copyright (2013-2018) David Janes
+ */
+
+"use strict";
+
+const _ = require("iotdb-helpers")
+
+const assert = require("assert")
+
+/**
+ */
+const xxx = {
+}
