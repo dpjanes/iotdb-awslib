842864fa2e6642a32056f08a3042161a0039807d
David Janes <david@consensas.com>
2019-07-20 13:56:18 -0400

    modernize

--- bin/s3-cat.js
+++ bin/s3-cat.js
@@ -12,42 +12,126 @@
 "use strict"
 
 const _ = require("iotdb-helpers")
+const fs = require("iotdb-fs")
 
 const assert = require("assert")
 const path = require("path")
-const fs = require("fs")
 
 const AWS = require("aws-sdk")
-const minimist = require('minimist')
+const minimist = require("minimist")
 
 const aws = require("../index")
 const awsd = {
 }
 
 const ad = minimist(process.argv.slice(2), {
-    boolean: [ "download", "json", ],
+    boolean: [ "download", "json", "help"],
     string: [ "profile", ],
 });
+
+const help = message => {
+    const name = "template"
+
+    if (message) {
+        console.log(`${name}: ${message}`)
+        console.log()
+    }
+
+    console.log(`usage: ${name} [options] <s3-url>...`)
+    console.log("")
+    console.log("Options:")
+    console.log("--json          format JSON nicely")
+    console.log("--download      download as file")
+    console.log("")
+    console.log("S3 URLs look like:")
+    console.log("")
+    console.log("  https://test-large.s3.amazonaws.com/")
+    console.log("  s3://test-large/Attachment-1.jpg")
+
+    process.exit(message ? 1 : 0)
+}
+
+if (ad.help) {
+    help()
+}
+if (!ad._.length) {
+    help("add at least one s3 url")
+}
 
 if (ad.profile) {
     awsd.profile = ad.profile
 }
 
-const action = (name) => ad._.indexOf(name) > -1;
+/**
+ */
+const _download = _.promise((self, done) => {
+    _.promise(self)
+        .validate(_download)
 
-ad._.forEach(s3_path => {
-    _.promise.make({
-        awsd: awsd,
-        path: s3_path,
-    })
-        .then(aws.initialize)
-        .then(aws.s3.initialize)
+        .add("path", path.basename(self.path))
+        .then(fs.write)
+
+        .end(done, self)
+})
+
+_download.method = "_download"
+_download.requires = {
+    document: [ _.is.String, _.is.Buffer ],
+}
+
+/**
+ */
+const _json = _.promise((self, done) => {
+    _.promise(self)
+        .validate(_json)
+
+        .make(sd => {
+            process.stdout.write(JSON.stringify(JSON.parse(sd.document), null, 2))
+            process.stdout.write("\n")
+        })
+
+        .end(done, self)
+})
+
+_json.method = "_json"
+_json.requires = {
+    document: [ _.is.String, _.is.Buffer ],
+}
+
+/**
+ */
+const _stdout = _.promise((self, done) => {
+    _.promise(self)
+        .validate(_stdout)
+
+        .then(fs.write.stdout)
+
+        .end(done, self)
+})
+
+_stdout.method = "_stdout"
+_stdout.requires = {
+    document: [ _.is.String, _.is.Buffer ],
+}
+
+/**
+ */
+const _get = _.promise((self, done) => {
+    _.promise(self)
+        .validate(_get)
+
         .then(aws.s3.parse_path)
         .then(aws.s3.get_object)
-        .then(_sd => {
+        .conditional(ad.download, _download)
+        .conditional(ad.json, _json)
+        .conditional(!ad.download && !ad.json, _stdout)
+
+        /*
+
+        .make(sd => {
             // console.log(_sd)
             if (ad.download) {
-                const name = path.basename(s3_path)
+                const name = path.basename(sd.path)
                 fs.writeFileSync(name, _sd.document)
 
                 console.log("+", "downloaded:", name)
@@ -58,8 +142,28 @@ ad._.forEach(s3_path => {
                 process.stdout.write(_sd.document)
             }
         })
-        .catch(error => {
-            console.log("#", _.error.message(error))
-        })
-    
+        */
+        .end(done, self)
 })
+
+_get.method = "_get"
+_get.requires = {
+    path: _.is.AbsoluteURL,
+}
+
+/**
+ */
+_.promise({
+    awsd: awsd,
+    paths: ad._,
+})
+    .then(aws.initialize)
+    .then(aws.s3.initialize)
+    .each({
+        method: _get,
+        inputs: "paths:path",
+    })
+
+    .catch(error => {
+        console.log("#", _.error.message(error))
+    })
